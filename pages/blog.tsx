508dade5aea67f2c5d5403c39523c3e79a8adc35
xxtereshko <19520678+xxtereshko@users.noreply.github.com>
2021-08-15 18:29:00 +0200

    Created blog page

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,14 +1,14 @@
 import Head from "next/head";
 
-export default function Home() {
+export default function Blog() {
   return (
     <>
       <Head>
-        <title>tereshko.net</title>
+        <title>Blog :: tereshko.net</title>
       </Head>
 
       <p className="p-6 md:p-7 m-4 md:m-8 md:text-lg bg-white rounded-2xl md:text-center">
-        tereshko.net 🥺
+        blog 🥺
       </p>
     </>
   );
