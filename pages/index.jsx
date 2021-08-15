d87f3caba54d7ea7614afa28ad9daf6881728662
xxtereshko <19520678+xxtereshko@users.noreply.github.com>
2021-08-15 15:46:24 +0200

    ts -> js

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,8 +5,11 @@ export default function Home() {
     <>
       <Head>
         <title>tereshko.net</title>
+        <meta name="description" content="Maxim Tereshko :: tereshko.net" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <p>tereshko.net :)</p>
+
+      <p>tereshko.net</p>
     </>
   );
 }
