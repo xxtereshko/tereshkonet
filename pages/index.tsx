90191b30c28846fb153ca94ac0bd7df8e94f9451
xxtereshko <19520678+xxtereshko@users.noreply.github.com>
2021-08-15 18:42:12 +0200

    Fixed favicon.ico

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <>
       <Head>
         <title>tereshko.net</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="images/favicon.ico" />
       </Head>
 
       <Link href="/">
