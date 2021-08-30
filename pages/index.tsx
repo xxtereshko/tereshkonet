89dbc16ecb0bfb317670401fb984be27049d88ad
xxtereshko <19520678+xxtereshko@users.noreply.github.com>
2021-08-31 01:31:53 +0200

    Updated index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,31 @@
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  return <p className="p-4">tereshko.net 🦄</p>
+  return (
+    <div className="px-4 py-8 space-y-4">
+      <p className="pb-3">Максим Дмитриевич Терешкó 🦄</p>
+      <p>
+        <a href="tel:+79097767696" className="text-blue-600">
+          +7 909 776 76 96
+        </a>
+      </p>
+      <p>4274 3200 3066 1923</p>
+      <p>
+        Telegram{' '}
+        <a href="https://t.me/xxtereshko" className="text-blue-600">
+          @xxtereshko
+        </a>
+      </p>
+      <p>
+        GitHub{' '}
+        <a href="https://github.com/xxtereshko" className="text-blue-600">
+          @xxtereshko
+        </a>
+      </p>
+
+      <p className="pt-3">Спасибо за внимание.</p>
+    </div>
+  )
 }
 
 export default Home
