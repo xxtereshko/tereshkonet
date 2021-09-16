8907dbe86bc294b818098d10ae931e326fd4abf4
xxtereshko <19520678+xxtereshko@users.noreply.github.com>
2021-09-16 20:05:22 +0200

    Fixed tailwind gray color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   darkMode: 'class',
   purge: ['./components/**/*.tsx', './pages/**/*.tsx', './public/**/*.html'],
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      red: colors.red,
+      yellow: colors.amber,
+      blue: colors.blue,
+    },
   },
   variants: {
     extend: {},
