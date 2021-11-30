2af7ad8574e6c332daf3e55007e2695c4aea8544
xxtereshko <19520678+xxtereshko@users.noreply.github.com>
2021-11-30 13:43:09 +0200

    Рефакторит Главную

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,22 @@
+import Image from 'next/image'
 import type { NextPage } from 'next'
+import tereshko from '../images/tereshko.jpeg'
 
 const Home: NextPage = () => {
   return (
-    <>
-      <p>tereshko.net</p>
-    </>
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <div className="flex flex-col items-center my-12 mx-4">
+        <Image
+          src={tereshko}
+          width={100}
+          height={100}
+          className="rounded-full"
+          alt={'tereshko.net'}
+        />
+        <p className="text-xl mt-4">Максим Терешко</p>
+        <p className="text-sm mt-1 text-gray-500">@xxtereshko</p>
+      </div>
+    </div>
   )
 }
 
