992b6d1885ac87f19b5f44a1af606f4d83057c55
xxtereshko <19520678+xxtereshko@users.noreply.github.com>
2021-12-28 21:06:45 +0200

    Исправляет главную

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,21 @@
-<h1>tereshko.net</h1>
+<svelte:head>
+	<title>Максим Терешко</title>
+</svelte:head>
 
-<style>
-	h1 {
-		padding: 1rem;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-</style>
+<main class="p-8 text-xl">
+	<h1 class="mb-8">Максим Терешко</h1>
+
+	<ul>
+		<li class="mb-4">
+			<a class="border-b-2 border-gray" href="https://t.me/xxtereshko">telegram</a>
+		</li>
+		<li class="mb-4">
+			<a class="border-b-2 border-gray" href="https://twitter.com/xxtereshko">twitter</a>
+		</li>
+		<li class="mb-4">
+			<a href="mailto:hey@tereshko.net">
+				💌 <span class="border-b-2 border-gray">hey@tereshko.net</span>
+			</a>
+		</li>
+	</ul>
+</main>
