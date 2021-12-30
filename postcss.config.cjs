329afd1d4d4044ef7cd23877da40ed3d3158b0ba
xxtereshko <19520678+xxtereshko@users.noreply.github.com>
2021-12-30 23:58:24 +0200

    Правит npm на yarn

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -2,11 +2,7 @@ const tailwindcss = require('tailwindcss')
 const autoprefixer = require('autoprefixer')
 
 const config = {
-	plugins: [
-		//Some plugins, like tailwindcss/nesting, need to run before Tailwind,
-		tailwindcss(),
-		autoprefixer()
-	]
+	plugins: [tailwindcss(), autoprefixer()]
 }
 
 module.exports = config
