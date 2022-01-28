68fe271c6083c00e723fe4f6c45662e500048522
xxtereshko <hey@tereshko.net>
2022-01-28 18:17:20 +0200

    Добавляет телефон

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -12,7 +12,7 @@
 </header>
 
 <main class="flex-1 mt-8">
-	<ul class="flex flex-col space-y-4 text-lg">
+	<ul class="flex flex-col space-y-3 text-lg">
 		<li>
 			☁️
 			<a class="link" href="https://t.me/xxtereshko" rel="noopener noreferrer">Telegram</a>
@@ -24,6 +24,10 @@
 		<li>
 			✉️
 			<a class="link" href="mailto:hey@tereshko.net" rel="noopener noreferrer">hey@tereshko.net</a>
+		</li>
+		<li>
+			✨
+			<a class="link" href="callto:+79097767696" rel="noopener noreferrer">+7 909 776-76-96</a>
 		</li>
 	</ul>
 </main>
