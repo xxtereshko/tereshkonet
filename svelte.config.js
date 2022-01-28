afcd2da57f82c65294f425d7e94ba7ed6e3a1943
xxtereshko <hey@tereshko.net>
2022-01-28 16:02:39 +0200

    Исправляет адаптер на static

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-vercel'
+// import adapter from '@sveltejs/adapter-vercel'
+import adapter from '@sveltejs/adapter-static'
 import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,7 +7,8 @@ const config = {
 	preprocess: [preprocess({})],
 	kit: {
 		adapter: adapter(),
-		target: '#svelte'
+		target: '#svelte',
+		floc: true
 	}
 }
 
