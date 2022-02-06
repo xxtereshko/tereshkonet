2417ff494a233fcc7bae32cb96afeb97c0b15e25
xxtereshko <hey@tereshko.net>
2022-02-06 02:30:59 +0200

    feature: easter

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,4 +2,45 @@
 	import '../app.css'
 </script>
 
-<slot />
+<div class="ref">здарова!</div>
+
+<header>
+	<h1 class="mb-2">Максим Терешко</h1>
+	<p class="text-sm text-gray">@xxtereshko</p>
+</header>
+
+<main class="flex-1 mt-8">
+	<slot />
+</main>
+
+<style>
+	.ref {
+		background-image: url('static/images/east.png');
+		background-position: 50% bottom;
+		background-repeat: no-repeat;
+		background-size: 85px;
+		height: 85px;
+		top: 0;
+		left: 0;
+		right: 0;
+		position: absolute;
+		transform: translateY(-85px) translateZ(0);
+		z-index: 100000;
+	}
+
+	.ref::after {
+		color: #000;
+		content: 'Привет!';
+		font-size: 14px;
+		left: 50%;
+		position: absolute;
+		top: 50%;
+		transform: translateX(50px);
+	}
+
+	@media (-webkit-min-device-pixel-ratio: 2), (min-resolution: 192dpi) {
+		.ref {
+			background-image: url('static/images/east@2x.png');
+		}
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,33 +1,22 @@
-<script>
-	let year = new Date(Date.now()).getFullYear()
-</script>
-
 <svelte:head>
 	<title>Максим Терешко</title>
 </svelte:head>
 
-<header>
-	<h1 class="mb-2">Максим Терешко</h1>
-	<p class="text-sm text-gray">@xxtereshko</p>
-</header>
-
-<main class="flex-1 mt-8">
-	<ul class="flex flex-col space-y-3 text-lg">
-		<li>
-			☁️
-			<a class="link" href="https://t.me/xxtereshko" rel="noopener noreferrer">Telegram</a>
-		</li>
-		<li>
-			👾
-			<a class="link" href="https://github.com/xxtereshko" rel="noopener noreferrer">GitHub</a>
-		</li>
-		<li>
-			✉️
-			<a class="link" href="mailto:hey@tereshko.net" rel="noopener noreferrer">hey@tereshko.net</a>
-		</li>
-		<li>
-			✨
-			<a class="link" href="callto:+79097767696" rel="noopener noreferrer">+7 909 776-76-96</a>
-		</li>
-	</ul>
-</main>
+<ul class="flex flex-col space-y-3 text-lg">
+	<li>
+		☁️
+		<a class="link" href="https://t.me/xxtereshko" rel="noopener noreferrer">Telegram</a>
+	</li>
+	<li>
+		👾
+		<a class="link" href="https://github.com/xxtereshko" rel="noopener noreferrer">GitHub</a>
+	</li>
+	<li>
+		✉️
+		<a class="link" href="mailto:hey@tereshko.net" rel="noopener noreferrer">hey@tereshko.net</a>
+	</li>
+	<li>
+		✨
+		<a class="link" href="callto:+79097767696" rel="noopener noreferrer">+7 909 776-76-96</a>
+	</li>
+</ul>
